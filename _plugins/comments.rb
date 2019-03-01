0c180fe3f15b492c8445cbf2f817fa77014e014f
Stig Otnes Kolstad <stig@stigok.com>
2019-03-01 04:02:14 +0100

    Add comments inclusion logic and layout

--- _plugins/comments.rb
+++ _plugins/comments.rb
@@ -1,22 +1,8 @@
 require 'digest'
 
-# Add an ID to all posts based on post date
+# Add a subject ID to all posts
 Jekyll::Hooks.register :posts, :pre_render do |post|
-  post.data['post_id'] = Digest::SHA256.hexdigest post.data['date'].strftime('%s')
+  post_hash = Digest::SHA256.hexdigest post.data['date'].strftime('%s')
+  post.data['comments_subject_id'] = post_hash
 end
 
-module Jekyll
-  module Comments
-    class Generator < Jekyll::Generator
-      def generate(site)
-        return
-        site.posts.each do |post|
-          id = post.data['post_id']
-          comments = site.collections.comments.select{ |com| com[:subject_id] == id }
-          post.data['comments'] = comments
-          puts "%d comments for post id %s" % [comments.count, id]
-        end
-      end
-    end # Generator
-  end # Comments
-end # Jekyll
