d5bbf37dae18561cd04745697856e0db4b977cbc
Stig Otnes Kolstad <stig@stigok.com>
2020-08-30 01:24:40 +0200

    add plugin: pymd

---
+++ _plugins/pymd.py
@@ -0,0 +1,73 @@
+import argparse
+import re
+import subprocess
+import sys
+
+
+def run_script(script_str):
+    """
+    Run `script_str` as a Python script and return the string results
+    """
+    p = subprocess.run(
+        ["python3", "-"],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.STDOUT,
+        input=script_str,
+        text=True,
+        check=True,
+    )
+    return p.stdout
+
+
+def process_text(text):
+    """
+    Find multiline scripts, tagged as python, within a markdown document.
+    If a script ends with the line `#eval`, the complete script will be
+    executed and the string results of the execution will be appended
+    to the markdown script section as a Python comment prefixed with `#`.
+    """
+
+    def _replacer(match):
+        script = match.group(1).strip()
+
+        try:
+            # Add output of the program
+            res = run_script(script)
+        except subprocess.CalledProcessError as e:
+            if e.returncode == 1:
+                res = e.output
+
+        # Build replacement string
+        out = "```python\n"
+
+        # Add the original source
+        out += script + "\n"
+
+        # Then print the results - commented
+        for line in filter(None, res.split("\n")):
+            out += "# %s\n" % (line)
+
+        out += "```"
+
+        return out
+
+    return re.sub(r"```python\n([^`]+)^#eval$\n```", _replacer, text, flags=re.M)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Execute inline Python code in a markdown document and add a comment with the execution results."
+    )
+    parser.add_argument(
+        "file", help="a markdown file with inline python scripts ending with #eval"
+    )
+    args = parser.parse_args()
+
+    if args.file == "-":
+        res = process_text(sys.stdin.read())
+    else:
+        with open(args.file, mode="r") as f:
+            text = f.read()
+            res = process_text(f.read())
+
+    print(res)
---
+++ _plugins/pymd.rb
@@ -0,0 +1,31 @@
+require 'open3'
+
+module Jekyll
+  module Pymd
+
+    class Generator < Jekyll::Generator
+      def generate(site)
+        return if ARGV.include?("--no-pymd")
+
+        @site = site
+        site.posts.docs.each do |post|
+          # Only run if pymd is explicitly specified
+          next unless post.data.has_key?('processors')
+          next unless post.data['processors'].include?('pymd')
+
+          puts "pymd processing: \"#{post.data['title']}\""
+          stdout, res = Open3.capture2("python3 #{__dir__}/pymd.py -", :stdin_data=>post.content)
+
+          if res != 0 then
+            #raise "pymd failed: #{res}"
+            Process.exit 1
+          end
+
+          post.content = stdout
+          post
+        end
+      end
+    end
+
+  end
+end
