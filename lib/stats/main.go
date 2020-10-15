024a24dab5387fd23bad8af079e7f470c09851d0
Stig Otnes Kolstad <stig@stigok.com>
2020-10-15 20:53:44 +0200

    finished

--- lib/stats/main.go
+++ lib/stats/main.go
@@ -1,24 +1,54 @@
 package main
 
 import (
-	"io"
+	"crypto/sha256"
+	"encoding/base64"
+	"fmt"
 	"log"
 	"net/http"
+	"time"
+
+	"github.com/gorilla/mux"
 )
 
+var db *Database
+var tinyGif []byte
+
 func main() {
-	helloHandler := func(w http.ResponseWriter, req *http.Request) {
-		io.WriteString(w, "Hello, world!\n")
+	// Tiniest gif I could find in 3 seconds
+	// http://probablyprogramming.com/2009/03/15/the-tiniest-gif-ever
+	gif, _ := base64.StdEncoding.DecodeString("R0lGODlhAQABAIABAP///wAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==")
+	tinyGif = gif
+
+	// Setup database
+	db = NewDatabase("mydb.sqlite.db")
+
+	// Main router
+	r := mux.NewRouter()
+	r.Methods("GET").PathPrefix("/visits/{post}").HandlerFunc(visitsHandler)
+
+	// HTTP server
+	srv := &http.Server{
+		Handler:      r,
+		Addr:         "0.0.0.0:8000",
+		WriteTimeout: 15 * time.Second,
+		ReadTimeout:  15 * time.Second,
 	}
 
-	http.HandleFunc("/hello", helloHandler)
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Printf("Listening on %s", srv.Addr)
+	srv.ListenAndServe()
 }
 
-func ImageOutput(w http.ResponseWriter, req *http.Request) {
+func visitsHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	post := vars["post"]
+	hash := fmt.Sprintf("%x", sha256.Sum256([]byte(r.RemoteAddr+r.Header.Get("User-Agent"))))
 
-}
+	// Log visit
+	log.Printf("visit: %s -> %s", hash, post)
+	db.RecordVisit(post, hash)
 
-func CountVisit(id string) {
-
+	// Return a tiny gif
+	w.Write(tinyGif)
+	return
 }
