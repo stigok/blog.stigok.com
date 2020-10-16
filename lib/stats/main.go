f49f2cfb72ad6ad99f8769b95de8856e3135e4c3
Stig Otnes Kolstad <stig@stigok.com>
2020-10-16 13:24:17 +0200

    move http to own file

--- lib/stats/main.go
+++ lib/stats/main.go
@@ -1,9 +1,6 @@
 package main
 
 import (
-	"crypto/sha256"
-	"encoding/base64"
-	"fmt"
 	"log"
 	"net/http"
 	"time"
@@ -12,59 +9,23 @@ import (
 )
 
 var db *Database
-var tinyGif []byte
 
 func main() {
-	// Tiniest gif I could find in 3 seconds
-	// http://probablyprogramming.com/2009/03/15/the-tiniest-gif-ever
-	gif, _ := base64.StdEncoding.DecodeString("R0lGODlhAQABAIABAP///wAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==")
-	tinyGif = gif
-
 	// Setup database
 	db = NewDatabase("mydb.sqlite.db")
 
-	// Main router
+	// Routes
 	r := mux.NewRouter()
-	r.Methods("GET").PathPrefix("/visits/{post}/record").HandlerFunc(recordVisitHandler)
-	r.Methods("GET").PathPrefix("/visits/{post}").HandlerFunc(getVisitHandler)
+	r.PathPrefix("/visits/").Handler(VisitsRouter(db))
 
 	// HTTP server
 	srv := &http.Server{
 		Handler:      r,
 		Addr:         "0.0.0.0:8000",
-		WriteTimeout: 15 * time.Second,
-		ReadTimeout:  15 * time.Second,
+		WriteTimeout: 3 * time.Second,
+		ReadTimeout:  3 * time.Second,
 	}
 
 	log.Printf("Listening on %s", srv.Addr)
 	srv.ListenAndServe()
-}
-
-func getVisitHandler(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
-	post := vars["post"]
-
-	cnt := db.GetVisitCount(post)
-
-	// Log visit
-	log.Printf("get visit: %d %s", cnt, post)
-
-	// Return count
-	w.Header().Add("Content-Type", "text/plain")
-	w.Write(tinyGif)
-}
-
-func recordVisitHandler(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
-	post := vars["post"]
-	hash := fmt.Sprintf("%x", sha256.Sum256([]byte(r.RemoteAddr+r.Header.Get("User-Agent"))))
-
-	// Log visit
-	log.Printf("visit: %s -> %s", hash, post)
-	db.RecordVisit(post, hash)
-
-	// Return a tiny gif
-	w.Header().Add("Content-Type", "image/gif")
-	w.Write(tinyGif)
-	return
 }
