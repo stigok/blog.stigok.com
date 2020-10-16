b97aabb4946112631f238541d2dfad74b5bc698b
Stig Otnes Kolstad <stig@stigok.com>
2020-10-16 13:24:17 +0200

    move http to own file

---
+++ lib/stats/http.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"crypto/sha256"
+	"encoding/base64"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+// Tiniest gif I could find in 3 seconds
+// http://probablyprogramming.com/2009/03/15/the-tiniest-gif-ever
+const gif_b64 string = "R0lGODlhAQABAIABAP///wAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw=="
+
+func VisitsRouter(db *Database) *mux.Router {
+	r := mux.NewRouter()
+
+	// Get visit
+	get := func(w http.ResponseWriter, r *http.Request) {
+		vars := mux.Vars(r)
+		post := vars["post"]
+
+		cnt := db.GetVisitCount(post)
+		log.Printf("get visit: %d %s", cnt, post)
+
+		w.Header().Add("Content-Type", "text/plain")
+		fmt.Fprintf(w, "%d", cnt)
+	}
+
+	// Record visit
+	hit := func(w http.ResponseWriter, r *http.Request) {
+		vars := mux.Vars(r)
+		post := vars["post"]
+		hash := fmt.Sprintf("%x", sha256.Sum256([]byte(r.RemoteAddr+r.Header.Get("User-Agent"))))
+
+		// Log visit
+		log.Printf("visit: %s -> %s", hash, post)
+		db.RecordVisit(post, hash)
+
+		// Return a tiny gif
+		w.Header().Add("Content-Type", "image/gif")
+		gif, _ := base64.StdEncoding.DecodeString(gif_b64)
+		w.Write(gif)
+		return
+	}
+
+	r.Methods("GET").PathPrefix("/{post}/get").HandlerFunc(get)
+	r.Methods("GET").PathPrefix("/{post}/hit").HandlerFunc(hit)
+
+	return r
+}
