57cb9401a8fecf9093df7946e1895dfd30a8b779
Stig Otnes Kolstad <stig@stigok.com>
2021-12-31 20:58:51 +0100

    chore: rebind local port for docker rootless mode

--- docker-build-run.sh
+++ docker-build-run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -ex
 docker build --network host -f .deploy/Dockerfile --tag blog.stigok.com .
-docker run --rm -it --network=host blog.stigok.com
+docker run --rm -it -p 127.0.0.1:8080:80 blog.stigok.com
